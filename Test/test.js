189b5eac4ba32e43cbbb4e1b76f8ac30ab4b70f8
mhamza123 <ihamza59@icloud.com>
2023-07-24 19:14:20 +0500

    Update test.js
    Now finding a movie and getting its description

--- Test/test.js
+++ Test/test.js
@@ -1,18 +1,20 @@
 const {By, Key, Builder} = require('selenium-webdriver');
 require('chromedriver');
 
-async function scrape(){
-    let driver = await new Builder().forBrowser('chrome').build();
-    await driver.get('https://www.imdb.com');
-    await driver.findElement(By.name('q')).sendKeys('Selenium', Key.RETURN);
-    let firstResult = await driver.findElement(By.css('h3'));
-    console.log(await firstResult.getAttribute('textContent'));
-
-}
 const prompt = require('prompt-sync')();
 const name = prompt('Please enter movie name:');
 const urlspace = encodeURIComponent(name);
-const url = `/find/?q=${urlspace}`;
-
+const url = `https://imdb.com/find/?q=${urlspace}`;
 console.log(url);
-//scrape();
+async function scrape(){
+    let driver = await new Builder().forBrowser('chrome').build();
+    await driver.get(url);
+    await driver.findElement(By.css(".ipc-metadata-list-summary-item")).click();
+    let description = await driver.findElement(By.xpath("//span[contains(@class,'sc-7193fc79-0')]"));
+    //await driver.findElement(By.name('q')).sendKeys('Selenium', Key.RETURN);
+    //let firstResult = await driver.findElement(By.css('h3'));
+    console.log(await description.getAttribute('textContent'));
+
+}
+
+scrape();
