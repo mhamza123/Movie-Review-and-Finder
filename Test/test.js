f8b7e1b2582c2a76e925b148fa952f242e15ebee
mhamza123 <ihamza59@icloud.com>
2023-07-25 17:32:30 +0500

    scraping working

--- Test/test.js
+++ Test/test.js
@@ -1,5 +1,7 @@
 const {By, Key, Builder} = require('selenium-webdriver');
 require('chromedriver');
+const axios = require('axios');
+const fs = require('fs');
 
 const prompt = require('prompt-sync')();
 const name = prompt('Please enter movie name:');
@@ -14,10 +16,17 @@ async function scrape(){
     //await driver.findElement(By.name('q')).sendKeys('Selenium', Key.RETURN);
     //let firstResult = await driver.findElement(By.css('h3'));
     console.log(await description.getAttribute('textContent'));
+    let review = await driver.findElement(By.xpath("//span[contains(@class,'sc-bde20123-1')]"));
+    console.log(await review.getAttribute('textContent'));
     await driver.findElement(By.css(".ipc-lockup-overlay__screen")).click();
     //let image = await driver.findElement(By.xpath("//img[contains(@class,'.sc-7c0a9e7c-0.fEIEer')]"));
     let image = await driver.findElement(By.css(".sc-7c0a9e7c-0.fEIEer"));
-    console.log(await image.getAttribute('src'));
+    imageUrl = await image.getAttribute('src');
+    console.log(imageUrl);
+    const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+    const fileName = 'movie_cover.jpg';
+    fs.writeFileSync(fileName, response.data);
+    console.log('Image downloaded successfully.');
 
 }
 
