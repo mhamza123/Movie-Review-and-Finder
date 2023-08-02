e85fa139e8b5b8775caab47878cea1c8d4ab9076
MehakFarhan <127510763+MehakFarhan@users.noreply.github.com>
2023-08-02 12:06:10 +0500

    updated

--- React/src/components/Movie.js
+++ React/src/components/Movie.js
@@ -1,31 +1,78 @@
-import React, { useState, useEffect } from "react";
+// Movie.js
+import React from "react";
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
-import "../styles/Movie.css"
+import "../styles/Movie.css";
 
 const Movie = () => {
-    let { id } = useParams();
-    const { error, isPending, data: movie } = useFetch(`http://localhost:8000/movie/${id}`);
+  const { id } = useParams();
+  const { error: movieError, isPending: moviePending, data: movie } = useFetch(`http://localhost:8000/movie/${id}`);
+  const { error: reviewError, isPending: reviewPending, data: reviews } = useFetch(`http://localhost:8000/reviews`);
+
+  if (movieError || reviewError) {
+    return <div className="movieDetails">{movieError || reviewError}</div>;
+  }
+
+  return (
+    <div className="movieDetails">
+      {moviePending || reviewPending ? (
+        <div className="movieDetails">Loading...</div>
+      ) : (
+        movie && (
+          <div className="movies">
+            <div className="movieDetails">
+              <div className="imageAndRest">
+                <img src={process.env.PUBLIC_URL + movie.img} alt={movie.title} />
+                <div className="restInfoAndButtons">
+                    <div className="nameAndButtons">
+                    <h2>{movie.name}</h2>
+                        <div className="details2">
+                            <button>Add to Favourite</button>
+                            <button>Add Review</button>
+                        </div>
+                    </div>
+                    <div className="info">
+                        <p>Description: {movie.Description}</p>
+                        <p>Release: {movie.release}</p>
+                        <p>Duration: {movie.duration}</p>
+                    </div>
+                </div>
+                
+                
+              </div>
+              
+            </div>
+            <div>
+            <div>
+                <h2>Reviews: </h2>
+            </div>
+            {reviews &&
+                reviews
+                  .map((review) => (
+                    (review.id === movie.id) && (
   
-    return (
-        <div className="movieDetails">
-        { error && <div className="movieDetails">{ error }</div> }
-        { isPending && <div className="movieDetails">Loading...</div> }
-        { movie &&  
-            <div className="movieDetails" key={movie.id} >
-                <div>
-                <img src={process.env.PUBLIC_URL + movie.img} />
-                </div>
-                <div className="info">
-                <p>Description: { movie.Description }</p>
-                <p>Release: { movie.release }</p>
-                <p>Duration: { movie.duration }</p>
-                </div>
-            </div> 
-        }
-        </div>
-       
-    );
-}
- 
+                        <div className="reviews">
+                            <div className="profile-circle">{review.username.charAt(0).toUpperCase()}</div>
+                            <div className="info">
+                                <p>Username: {review.username}</p>
+                                <p>Review: {review.text}</p>
+                                <p>Star: {getStarRating(review.star)}</p>
+                            </div>
+                        </div>
+                    )
+            ))}
+            </div>
+          </div>
+        )
+      )}
+    </div>
+  );
+};
+
 export default Movie;
+
+const getStarRating = (rating) => {
+    const filledStars = "★".repeat(rating);
+    const emptyStars = "☆".repeat(5 - rating);
+    return filledStars + emptyStars;
+};
